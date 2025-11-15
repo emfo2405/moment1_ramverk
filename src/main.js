7f2a2501a737d33b8068851fad8fcf2321b6e5a4
emfo2405 <emfo2405@student.miun.se>
2025-11-15 22:41:42 +0100

    kommentarer tillagda

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-    let menuSymbol = document.getElementById("menuSymbol");
-    let smallMenu = document.getElementById("smallMenu");
+/*Hämtar element från HTML-filen, menyknapp och meny för små skärmar */
+let menuSymbol = document.getElementById("menuSymbol");
+let smallMenu = document.getElementById("smallMenu");
 
+/*Skapar en funktion för att visa eller dölja menyn */
 function hamburgerMenu() {
+    /*Funktionen ska ändra mellan ursprungsläge och hidden */
     smallMenu.classList.toggle("hidden");
 }
 
+/*Funktionen kopplas till en knapp och används vid klick */
 menuSymbol.addEventListener("click", hamburgerMenu);
 
 
--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,16 @@
-   let hamburgerSymbol = document.getElementById("hamburgerSymbol");
-    let smallScreenMenu = document.getElementById("smallScreenMenu");
+/*hämtar in HTML-element */
+let hamburgerSymbol = document.getElementById("hamburgerSymbol");
+let smallScreenMenu = document.getElementById("smallScreenMenu");
 
+/*Skapar funktion för att visa eller dölja menyn */
 function displayMenu() {
-    if(smallScreenMenu.style.display === "block") {
+    /*Om menyn visas ska den döljas och tvärtom, när funktionen körs */
+    if (smallScreenMenu.style.display === "block") {
         smallScreenMenu.style.display = "none";
     } else {
         smallScreenMenu.style.display = "block";
     }
 }
 
+/*Sätter funktionen på en knapp som körs vid klick */
 hamburgerSymbol.addEventListener("click", displayMenu); 
